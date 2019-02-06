9003196953719e5f8516fb9e4fdf397b0845ca6d
Stephane Tombeur <stombeur@users.noreply.github.com>
2019-02-06 10:02:17 +0100

    Create talk-html-to-me.js

---
+++ talk-html-to-me.js
@@ -0,0 +1,25 @@
+
+//console.table(voices);
+var button = document.getElementById('clickme');
+button.disabled = true;
+
+window.speechSynthesis.onvoiceschanged = function() {
+    window.speechSynthesis.getVoices();
+    button.disabled = false;
+};
+
+function vindVlaamseEllen(voice) {
+    return voice.voiceURI === "Ellen";
+}
+
+button.onclick = function() {
+    var txttospeak = document.getElementById('inputtext').value;
+    var synth = window.speechSynthesis;
+    var voices = synth.getVoices();
+    console.table(voices);
+    var speak = new SpeechSynthesisUtterance(txttospeak);
+    var ellen = voices.find(vindVlaamseEllen);
+    //console.log(ellen);
+    speak.voice = ellen;
+    synth.speak(speak);
+};
